d2c0c384e83e1be0e545dfaeb1ea7e9782ea0756
Vladimir Dimitrov <vl.dimitrov89@gmail.com>
2021-08-03 11:20:38 +0300

    Added portrait of character

--- src/components/Character/CharacterInfo.js
+++ src/components/Character/CharacterInfo.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Jobs from '../Jobs/Jobs';
-import { Container, Grid, Box } from '@material-ui/core';
+import { Container, Grid, Box, Avatar } from '@material-ui/core';
+import Gear from '../Gear/Gear';
 
 
 const CharacterInfo = function(props) {
+    console.log(props);
     return(
         <div>
-            <Grid container spacing={2}>
-                <Grid item xs={6} sm={3}>
-                    <Container maxWidth="sm">
-                        <Box bgcolor="text.primary" color='white'>
-                            <Jobs classesInfo={props.characterData.ClassJobs} />
+            <Container maxWidth="xl">
+                <Grid container spacing={2}>
+                    <Grid item xs={6} sm={3}>
+                        <Container maxWidth="sm">
+                            <Box bgcolor="text.primary" color='white'>
+                                <Jobs classesInfo={props.characterData.ClassJobs} />
+                            </Box>
+                        </Container >
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                        <Box>
+                            <img src={props.characterData.Portrait} alt={props.characterData.Name} />
                         </Box>
-                    </Container >
+                        <Gear />
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} sm={3}>
-                
-                </Grid>
-            </Grid>
-            
+            </Container>
         </div>
     );
 }
--- src/components/Gear/Gear.js
+++ src/components/Gear/Gear.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+//Component for showing character's gear
 const Gear = function(props) {
-
+    return (
+        <div></div>
+    );
 }
 
 export default Gear;
